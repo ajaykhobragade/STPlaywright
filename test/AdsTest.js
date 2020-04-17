94368688de39537cf79c7feb9c4c76e497cec799
khobraj <ajay.khobragade@startribune.com>
2020-04-17 09:27:10 -0500

    Add more code for Ads

--- test/AdsTest.js
+++ test/AdsTest.js
@@ -5,40 +5,87 @@ const playwright = require('playwright');
     //Code execution happens within here
     const browser = await playwright["chromium"].launch({
         headless: false,
-        devtools: true
+        devtools: false
     });
 
     //context
     const context = await browser.newContext();
 
-    //navigate to the page
+    //navigate to the newpage
     const page = await context.newPage();
 
-    page.on('load', () => console.log('Page loading!'));
+    page.on('load', () => console.log('Page loaded!'));
 
-    //navigate to the page
-    await page.goto("https://www.startribune.com/");
+    //navigate to the homepage
+    await page.goto("https://www.startribune.com/?sz=/7932/website/web_test/test67");
 
-    var waitPeriod = 10;
+    var waitPeriod = 1;
     await page.waitForResponse(response => {
         console.log("Starting to wait... " + waitPeriod);
         waitPeriod++;
-        return response.request().resourceType() === "image"
+        return response.request().resourceType() === "xhr"
     })
 
-    //check image existance
-    //await page.type('[xpath=//*[@id="aw0"]/amp-img/img]')
-    //await page.
-    
-    //sample console log
-    page.on('console', msg => {
-        for (let i = 0; i < msg.args().length; ++i)
-          console.log(`${i}: ${msg.args()[i]}`);
-      });
-    page.evaluate(() => console.log('looking for Ads!'));
+    //check Ads location existance
+    if (await page.$('div.l-home-top-ad') !== null) console.log('DFP Direct Ad is found...confirming Ad is filling up');
+    else console.log('DFP Direct Ad is not found');
 
-    //screenshot
-    await page.screenshot({path: `ea-${Date.now}.png`});
+    if (await page.$('#zone-2-block-2-vertical') !== null) console.log('DFP Indirect block-2-vertical Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-2-block-3-vertical') !== null) console.log('DFP Indirect block-3-vertical Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-1C-block-1-native') !== null) console.log('DFP Indirect block-1-native Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-2-block-4-vertical') !== null) console.log('DFP Indirect block-4-vertical Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-none-block-2-leaderboard') !== null) console.log('DFP Indirect block-2-leaderboard Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-3-block-5-square') !== null) console.log('DFP Indirect block-5-square Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-3-block-9-square') !== null) console.log('DFP Indirect block-9-square Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-3-block-13-square') !== null) console.log('DFP Indirect block-13-square Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-3-block-17-square') !== null) console.log('DFP Indirect block-17-square Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+
+    if (await page.$('#zone-none-block-3-leaderboard') !== null) console.log('DFP Indirect block-3-leaderboard Ad is found...confirming Ad is filling up');
+    else console.log('DFP Indirect Ad is not found');
+    //check Ads location existance
+
+    //fullpage screenshot
+    await scrollFullPage(page);
+
+    async function scrollFullPage(page) {
+        await page.evaluate(async () => {
+          await new Promise(resolve => {
+            let totalHeight = 0;
+            const distance = 100;
+            const timer = setInterval(() => {
+              const scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+              
+              if (totalHeight >= scrollHeight){
+                clearInterval(timer);
+                resolve();
+              }
+            }, 100);
+          });
+        });
+      }
+
+    await page.screenshot({ 
+        path: 'startribune_homepage.png',
+        fullPage : true });
     
     //close browser
     await browser.close();
