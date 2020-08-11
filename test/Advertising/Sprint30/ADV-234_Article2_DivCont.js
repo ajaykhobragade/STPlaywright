b04eb5678f25555bd86562ddede55d9c0fff37c4
khobraj <ajay.khobragade@startribune.com>
2020-08-11 14:43:49 -0500

    adding div counts lazy loads scripts

---
+++ test/Advertising/Sprint30/ADV-234_Article2_DivCont.js
@@ -0,0 +1,128 @@
+const playwright = require('playwright');
+const { LazyLoad_Article2_URL_P } = require('./urls');
+const { LazyLoad_Article2_URL_S } = require('./urls');
+
+(async () => {
+
+  //#region launch browsers, contexts and page
+    for (const browsertype of ['chromium']) {
+      const browser = await playwright[browsertype].launch({
+          headless: true,
+          devtools: false
+      });
+    const context1 = await browser.newContext();
+    const context2 = await browser.newContext();
+    const page1 = await context1.newPage();
+    const page2 = await context2.newPage();
+  //#endregion
+  
+  //#region navigate to the pages
+    await page1.goto(LazyLoad_Article2_URL_P, {
+        waitUntil: 'load',
+        timeout: 0
+      });
+    console.log('Prod URL is loaded!');
+    
+  //#region login 
+    await page1.click('a.nav-utility-btn.btn-login.login-js');
+    await page1.type('//*[@id="username"]', 'ajay.khobragade@startribune.com');
+    await page1.type('//*[@id="password"]', 'mystrib123');
+    await page1.click('//*[@id="login-form"]/fieldset[3]/div/input[3]');
+    await page1.waitForNavigation({waitUntil: 'load'});
+
+    await page1.waitForResponse(response => {
+            waitPeriod++;
+            return response.request().resourceType() === "xhr"
+        })
+  //#endregion
+
+  //#region scrollfullpage
+    await scrollFullPage(page1);
+
+    async function scrollFullPage(page1) {
+        await page1.evaluate(async () => {
+          await new Promise(resolve => {
+            let totalHeight = 0;
+            const distance = 100;
+            const timer = setInterval(() => {
+              const scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+              
+              if (totalHeight >= scrollHeight){
+                clearInterval(timer);
+                resolve();
+              }
+            }, 100);
+          });
+        });
+      }
+  //#endregion
+
+    var waitPeriod = 10;
+    await page1.waitForResponse(response => {
+        waitPeriod++;
+        return response.request().resourceType() === "xhr"
+    });
+
+    await page2.goto(LazyLoad_Article2_URL_S, {
+        waitUntil: 'load',
+        timeout: 0
+      });
+    console.log('Stage URL is loaded!');
+    
+  //#region login 
+    await page2.click('a.nav-utility-btn.btn-login.login-js');
+    await page2.type('//*[@id="username"]', 'ajay.khobragade@startribune.com');
+    await page2.type('//*[@id="password"]', 'mystrib123');
+    await page2.click('//*[@id="login-form"]/fieldset[3]/div/input[3]');
+    await page2.waitForNavigation({waitUntil: 'load'});
+
+    await page2.waitForResponse(response => {
+            waitPeriod++;
+            return response.request().resourceType() === "xhr"
+        })
+  //#endregion
+
+    //#region scrollfullpage
+    await scrollFullPage(page2);
+    //#endregion
+    
+    var waitPeriod = 10;
+    await page2.waitForResponse(response => {
+        waitPeriod++;
+        return response.request().resourceType() === "xhr"
+    });
+
+  //#endregion
+
+  //#region compare ad div counts
+    const divsCounts1 = await page1.$$eval('.dfp-ad-container', (divs) => divs.length);
+    console.log('The total number of Ad div counts with matching class on prod is ' + divsCounts1);
+
+    const divsCounts2 = await page2.$$eval('.dfp-ad-container', (divs) => divs.length);
+    console.log('The total number of Ad div counts with matching class on stage is ' + divsCounts2);
+
+    if (divsCounts1==divsCounts2) console.log ('divCounts on prod and stage is matched');
+    else console.log('divCounts on prod and stage is NOT matched');
+  //#endregion
+
+  //#region capture fullpage screenshot
+    await page1.screenshot({ 
+      path: `Prod-ADV-234_Article2_DivCont-${browsertype}.png`,
+      fullPage : true });
+    console.log('Screenshot captured on ' + browsertype + ' browser!');
+
+    await page2.screenshot({ 
+      path: `Stage-ADV-234_Article2_DivCont-${browsertype}.png`,
+      fullPage : true });
+    console.log('Screenshot captured on ' + browsertype + ' browser!');
+  //#endregion
+
+  //#region close contexts, browser
+    await context1.close();
+    await context2.close();
+    browser.close();
+  //#endregion
+    }
+  })();
--- test/Advertising/Sprint30/urls.js
+++ test/Advertising/Sprint30/urls.js
@@ -11,11 +11,17 @@ module.exports = {
     LazyLoad_URL_S: 'https://stage-www.startribune.com/walz-mandates-masks-indoors-across-state/571861471/?segment=Subscriber&sz=%2F7932%2Fwebsite%2Fweb_test%2Ftest67',
     LazyLoadOff_URL_P: 'https://www.startribune.com/walz-mandates-masks-indoors-across-state/571861471/?segment=Subscriber&sz=%2F7932%2Fwebsite%2Fweb_test%2Ftest67&featureOff=Subscriber%20Lazy%20Load',
     LazyLoadOff_URL_S: 'https://stage-www.startribune.com/walz-mandates-masks-indoors-across-state/571861471/?segment=Subscriber&sz=%2F7932%2Fwebsite%2Fweb_test%2Ftest67&featureOff=Subscriber%20Lazy%20Load',
+    LazyLoad_HP_URL_P: 'https://www.startribune.com/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
     LazyLoad_HP_URL_S: 'https://stage-www.startribune.com/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
+    LazyLoad_Article_URL_P: 'https://www.startribune.com/police-umbrella-man-was-a-white-supremacist-trying-to-incite-george-floyd-rioting/571932272/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
     LazyLoad_Article_URL_S: 'https://stage-www.startribune.com/police-umbrella-man-was-a-white-supremacist-trying-to-incite-george-floyd-rioting/571932272/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
+    LazyLoad_Article2_URL_P: 'https://www.startribune.com/coronavirus-covid-19-minnesota-tracker-map-county-data/568712601/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
     LazyLoad_Article2_URL_S: 'https://stage-www.startribune.com/coronavirus-covid-19-minnesota-tracker-map-county-data/568712601/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
+    LazyLoad_SectionFront_URL_P: 'https://www.startribune.com/local/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
     LazyLoad_SectionFront_URL_S: 'https://stage-www.startribune.com/local/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
+    LazyLoad_SectionFront2_URL_P: 'http://www.startribune.com/sports/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
     LazyLoad_SectionFront2_URL_S: 'http://stage-www.startribune.com/sports/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
+    LazyLoad_SectionFront3_URL_P: 'http://www.startribune.com/variety/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
     LazyLoad_SectionFront3_URL_S: 'http://stage-www.startribune.com/variety/?sz=/7932/website/web_test/test67/?featureOff=Bounce-X',
   }
 
